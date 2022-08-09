88b23f327220b3fe002d3b6d99271e9e70a18f94
bseefeldt37 <benjamin.seefeldt@outlook.com>
2022-08-09 16:11:11 -0500

    Added functions for the script.

--- Requirements1/prompts.ps1
+++ Requirements1/prompts.ps1
@@ -1,7 +1,7 @@
 # By: Benjamin Seefeldt Student ID: 006954697
 
 #VARIABLE DECLARATION
-
+$error0 = "Invalid selection, please choose from 1-5."
 
 #FUNCTION DECLARATION
 function Open-Menu {
@@ -12,9 +12,33 @@ function Open-Menu {
     2: Folder contents
     3: Current CPU and memory usage
     4: Running processes
-    5: Exit"
-    Read-Host -Prompt "Selection: __"
+    5: Exit
+    "
 }
-
+function Show-Logs {
+    Get-Date -DisplayHint Date
+    Get-ChildItem ./ -Filter *.log | Select-Object Name | Sort-Object -Property Name | Out-File DailyLog.txt
+    Get-Content ./DailyLog.txt
+}
+function Show-Files {
+    Get-ChildItem ./ | Select-Object Name,LastWriteTime,Size | Sort-Object | Format-Table | Out-File C916contents.txt
+    Get-Content ./C916contents.txt
+}
+function Show-Perf {
+    Get-Counter '\Processor(_Total)\% Processor Time'
+    Get-Counter '\Memory\Available MBytes'
+}
+Get-Process *
 #BODY
 Open-Menu
+switch (Read-Host -Prompt "Selection") {
+    {$_ -lt 1} {Write-Host $error0 -ForegroundColor Red}
+    {$_ -gt 5} {Write-Host $error0 -ForegroundColor Red}
+    1 {Show-Logs}
+    2 {Show-Files}
+    3 {Show-Perf}
+    4 {Write-Host "You selected 4"}
+    5 {Write-Host "Goodbye." -ForegroundColor Red
+        Exit-PSHostProcess}
+    default {Write-Host "Invalid selection, please choose from 1-5."}
+}
