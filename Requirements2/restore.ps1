2596a64c368fe1e19e5da9219020f8baaaaa0099
Benjamin Seefeldt <benjamin.seefeldt@outlook.com>
2022-09-08 19:24:46 -0700

    Created and validated ADUser import

---
+++ Requirements2/restore.ps1
@@ -0,0 +1,24 @@
+# By: Benjamin Seefeldt Student ID: 006954697
+
+
+### BODY ###
+
+# Creation of OU
+New-ADOrganizationalUnit finance
+
+# Account import
+$financemembers = Import-Csv .\financePersonnel.csv
+$OUpath = Get-ADOrganizationalUnit -Filter 'name -like "finance"'
+
+foreach ($member in $financemembers) {
+    $firstname = $member.First_Name
+    $lastname = $member.Last_Name
+    $username = $member.samAccount
+    $displayname = $firstname + " " + $lastname
+
+    New-ADUser -Name $displayname -GivenName $firstname -Surname $lastname -DisplayName $displayname -PostalCode $member.PostalCode -OfficePhone $member.OfficePhone -MobilePhone $member.MobilePhone -Path $OUpath.DistinguishedName -SamAccountName $member.samAccount
+
+}
+
+git config --global user.email "benjamin.seefeldt@outlook.com"
+git config --global user.name "Benjamin Seefeldt"
