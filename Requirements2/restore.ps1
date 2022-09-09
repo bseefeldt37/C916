6ba9ee5c7fc4c2aa28076d9b94123204da8e51c3
Benjamin Seefeldt <benjamin.seefeldt@outlook.com>
2022-09-08 19:34:23 -0700

    Added AD account creation

--- Requirements2/restore.ps1
+++ Requirements2/restore.ps1
@@ -13,7 +13,6 @@ $OUpath = Get-ADOrganizationalUnit -Filter 'name -like "finance"'
 foreach ($member in $financemembers) {
     $firstname = $member.First_Name
     $lastname = $member.Last_Name
-    $username = $member.samAccount
     $displayname = $firstname + " " + $lastname
 
     New-ADUser -Name $displayname -GivenName $firstname -Surname $lastname -DisplayName $displayname -PostalCode $member.PostalCode -OfficePhone $member.OfficePhone -MobilePhone $member.MobilePhone -Path $OUpath.DistinguishedName -SamAccountName $member.samAccount
